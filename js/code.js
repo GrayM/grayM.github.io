b3b2a751e71a9481c26e198fa1ca731cc939c5dd
grayM <yamawarrior@gmail.com>
2014-09-17 15:40:34 -0500

    icon scroll

--- js/code.js
+++ js/code.js
@@ -1,31 +1,48 @@
 $(window).scroll(function(){
     $(".main-header").css("top",Math.max(0,250-$(this).scrollTop()));
-
+    // $(".icons").css("bottom",Math.max(0,-500-$(this).scrollTop()));
     // $("p").css("position",Math.max(0,250-$("p").scrollTop()));
 
-    var s = $(".background");
-    var h = $(".main-header");
+});
 
+$(document).ready(function() {
+    // var s = $(".background");
+    var h = $("h2, h4");
+    var i = $(".icons");
     var isSticking = false;
 
     $(window).scroll(function() {
         var windowpos = $(window).scrollTop();
-        var pos = s.position();
+        // var pos = s.position();
         var posh = h.position();
-        var shouldSetSticky0 = windowpos >= posh.top;
-        var shouldSetSticky = windowpos >= pos.top + 600;
+        // var posi = i.position();
+
+        var shouldSetSticky0 = windowpos >= posh.top + 700;
+        // var shouldSetSticky = windowpos >= pos.top + 600;
+        // var shouldSetSticky1 = windowpos >= posi.top;
 
         if (shouldSetSticky0 && !isSticking) {
-            isSticking = true;
-        } else if(!shouldSetSticky0 && isSticking){
-            isSticking = false;
-        } else if (shouldSetSticky0 && isSticking) {
+            i.removeClass("icons");
+            i.addClass("iconMove");
             h.addClass("vanish");
             isSticking = true;
-        } else if (!shouldSetSticky0 && !isSticking) {
+        } else if (!shouldSetSticky0 && isSticking) {
+            i.removeClass("iconMove");
+            i.addClass("icons");
             h.removeClass("vanish");
             isSticking = false;
         }
+        // } else if (shouldSetSticky0 && !isSticking) {
+        //     isSticking = true;
+        // } else if(!shouldSetSticky0 && isSticking){
+        //     isSticking = false;
+        // } else if (shouldSetSticky0 && isSticking) {
+        //     h.addClass("vanish");
+        //     isSticking = true;
+        // } else if (!shouldSetSticky0 && !isSticking) {
+        //     h.removeClass("vanish");
+        //     isSticking = false;
+        // } 
 
     });
 });
