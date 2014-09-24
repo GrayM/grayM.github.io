3f35e94fec3f7c4f604620183a3adafa5c71b60e
grayM <yamawarrior@gmail.com>
2014-09-23 20:29:23 -0500

    reorganize divs

--- js/code.js
+++ js/code.js
@@ -1,3 +1,45 @@
+//  $(window).scroll(function(){
+//     var h = $("h2, h4");
+//     var i = $(".icons");
+//     var b = $(".fa-angle-double-down");
+//     var windowpos = $(window).scrollTop();
+//     var posh = h.position();
+//     var shouldSetSticky0 = windowpos >= posh.top + 650;
+//     var isSticking = false;
+//     var is_touch_device = 'ontouchstart' in document.documentElement;
+
+//     var device = navigator.userAgent.toLowerCase();
+//     var ios = device.match(/(iphone|ipod|ipad)/);
+
+//     if (ios || is_touch_device) {
+//         //ADD CLASSES HERE THAT WILL ONLY APPLY TO IOS
+//         $(".parallax").addClass("iOSBackground");
+//         $(".iconMove").addClass("iosIcons");
+//         h.addClass("mobile-header");
+//     }
+
+//     if (shouldSetSticky0 && !isSticking) {
+//         i.removeClass("icons");
+//         i.addClass("iconMove");
+//         b.removeClass("fa-angle-double-down");
+//         h.addClass("vanish");
+//         isSticking = true;
+//     } else if (!shouldSetSticky0 && isSticking) {
+//         i.removeClass("iconMove");
+//         i.addClass("icons");
+//         b.addClass("fa-angle-double-down");
+//         h.removeClass("vanish");
+//         isSticking = false;
+//     }
+
+// });
+
+
+//  $(document).ready(function(){
+//     $(".main-header").css("top", Math.max(0, 250 - $(this).scrollTop()));
+// });
+
+
 $(document).ready(function() {
 
     var h = $("h2, h4");
@@ -5,43 +47,37 @@ $(document).ready(function() {
     var b = $(".fa-angle-double-down");
     var is_touch_device = 'ontouchstart' in document.documentElement;
     var isSticking = false;
-    var ios = device.match(/(iphone|ipod|ipad)/);
+    
+    if (!is_touch_device) {
+        $(window).scroll(function() {
+            var windowpos = $(window).scrollTop();
+            var posh = h.position();
+            var shouldSetSticky0 = windowpos >= posh.top + 650;
+            var device = navigator.userAgent.toLowerCase();
+            var ios = device.match(/(iphone|ipod|ipad)/);
 
-    if (ios) {
-        //ADD CLASSES HERE THAT WILL ONLY APPLY TO IOS
-        $(".parallax").addClass("iOSBackground");
-        $(".iconMove").addClass("iosIcons");
-        h.addClass("mobile-header");
+            $(".main-header").css("top", Math.max(0, 250 - $(this).scrollTop()));
+
+
+
+
+            if (shouldSetSticky0 && !isSticking) {
+                i.removeClass("icons");
+                i.addClass("iconMove");
+                b.removeClass("fa-angle-double-down");
+                h.addClass("vanish");
+                isSticking = true;
+            } else if (!shouldSetSticky0 && isSticking) {
+                i.removeClass("iconMove");
+                i.addClass("icons");
+                b.addClass("fa-angle-double-down");
+                h.removeClass("vanish");
+                isSticking = false;
+            }
+            
+
+        });
     }
-    $(window).scroll(function() {
-        var windowpos = $(window).scrollTop();
-        var posh = h.position();
-        var shouldSetSticky0 = windowpos >= posh.top + 650;
-        var device = navigator.userAgent.toLowerCase();
-
-
-        $(".main-header").css("top", Math.max(0, 250 - $(this).scrollTop()));
-
-
-
-
-        if (shouldSetSticky0 && !isSticking) {
-            i.removeClass("icons");
-            i.addClass("iconMove");
-            b.removeClass("fa-angle-double-down");
-            h.addClass("vanish");
-            isSticking = true;
-        } else if (!shouldSetSticky0 && isSticking) {
-            i.removeClass("iconMove");
-            i.addClass("icons");
-            b.addClass("fa-angle-double-down");
-            h.removeClass("vanish");
-            isSticking = false;
-        }
-
-
-    });
-
 
 
 
