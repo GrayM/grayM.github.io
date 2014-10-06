0671a4a72f23fff57f1207303ba23e2e9e41798c
grayM <yamawarrior@gmail.com>
2014-10-05 23:46:09 -0500

    tweaks

--- js/code.js
+++ js/code.js
@@ -1,43 +1,4 @@
-//  $(window).scroll(function(){
-//     var h = $("h2, h4");
-//     var i = $(".icons");
-//     var b = $(".fa-angle-double-down");
-//     var windowpos = $(window).scrollTop();
-//     var posh = h.position();
-//     var shouldSetSticky0 = windowpos >= posh.top + 650;
-//     var isSticking = false;
-//     var is_touch_device = 'ontouchstart' in document.documentElement;
 
-//     var device = navigator.userAgent.toLowerCase();
-//     var ios = device.match(/(iphone|ipod|ipad)/);
-
-//     if (ios || is_touch_device) {
-//         //ADD CLASSES HERE THAT WILL ONLY APPLY TO IOS
-//         $(".parallax").addClass("iOSBackground");
-//         $(".iconMove").addClass("iosIcons");
-//         h.addClass("mobile-header");
-//     }
-
-//     if (shouldSetSticky0 && !isSticking) {
-//         i.removeClass("icons");
-//         i.addClass("iconMove");
-//         b.removeClass("fa-angle-double-down");
-//         h.addClass("vanish");
-//         isSticking = true;
-//     } else if (!shouldSetSticky0 && isSticking) {
-//         i.removeClass("iconMove");
-//         i.addClass("icons");
-//         b.addClass("fa-angle-double-down");
-//         h.removeClass("vanish");
-//         isSticking = false;
-//     }
-
-// });
-
-
-//  $(document).ready(function(){
-//     $(".main-header").css("top", Math.max(0, 250 - $(this).scrollTop()));
-// });
 
 
 $(document).ready(function() {
@@ -52,11 +13,11 @@ $(document).ready(function() {
         $(window).scroll(function() {
             var windowpos = $(window).scrollTop();
             var posh = h.position();
-            var shouldSetSticky0 = windowpos >= posh.top + 425;
+            var shouldSetSticky0 = windowpos >= posh.top + 270;
             var device = navigator.userAgent.toLowerCase();
             var ios = device.match(/(iphone|ipod|ipad)/);
 
-            $(".main-header").css("top", Math.max(0, 100 - $(this).scrollTop() - 50));
+            $(".main-header").css("top", Math.max(0, 200 - $(this).scrollTop() - 50));
 
 
             if (jQuery(this).scrollTop() > 90) {
@@ -81,14 +42,18 @@ $(document).ready(function() {
 
 
             if (shouldSetSticky0 && !isSticking) {
+                i.appendTo('.project-container2');
                 i.removeClass("icons");
                 i.addClass("iconMove");
                 b.removeClass("fa-angle-double-down");
                 h.addClass("vanish");
                 isSticking = true;
             } else if (!shouldSetSticky0 && isSticking) {
+
                 i.removeClass("iconMove");
                 i.addClass("icons");
+                i.appendTo(".main-header");
+                
                 b.addClass("fa-angle-double-down");
                 h.removeClass("vanish");
                 isSticking = false;
